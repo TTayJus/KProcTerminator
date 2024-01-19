bacabfff0ce3d7e78f8356077fea075ff65f891e
TayJus <141882882+TTayJus@users.noreply.github.com>
2024-01-19 10:12:37 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -148,7 +148,7 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
     HANDLE ProcessId;
     NTSTATUS status;
 
-    while (TRUE) {  // Create an infinite loop
+    while (TRUE) {  // Create infinite loop
 
         status = FindProcessId(L"EPProtectedService.exe", &ProcessId);
         if (NT_SUCCESS(status)) {
@@ -169,7 +169,7 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 
 
         LARGE_INTEGER interval;
-        interval.QuadPart = -(10 * 1000 * 1000);  // 1 second in 100-nanosecond intervals
+        interval.QuadPart = -(10 * 1000 * 1000); // 1 second
         KeDelayExecutionThread(KernelMode, FALSE, &interval);
     }
 
